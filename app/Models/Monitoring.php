8b89c2e1cdffb52e322db1980c7ae3711aa8b307
Andik Kurniawan <andikk362@gmail.com>
2024-06-17 09:41:23 +0700

    add booted function to Monitoring model

--- app/Models/Monitoring.php
+++ app/Models/Monitoring.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
-use App\Models\Analysis;
+use App\Models\Monitoring;
+use App\Models\ActivityLog;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -22,48 +24,72 @@ class Monitoring extends Model
         return $this->belongsTo(Parameter::class);
     }
 
+    protected static function booted()
+    {
+        static::created(function ($monitoring) {
+            ActivityLog::create([
+                'user_id' => Auth::id(),
+                'description' => "Monitoring '{$monitoring->material->name}' with ID '{$monitoring->id}' was created.",
+            ]);
+        });
+
+        static::updated(function ($monitoring) {
+            ActivityLog::create([
+                'user_id' => Auth::id(),
+                'description' => "Monitoring '{$monitoring->material->name}' with ID '{$monitoring->id}' was updated.",
+            ]);
+        });
+
+        static::deleted(function ($monitoring) {
+            ActivityLog::create([
+                'user_id' => Auth::id(),
+                'description' => "Monitoring '{$monitoring->material->name}' with ID '{$monitoring->id}' was deleted.",
+            ]);
+        });
+    }
+
     public static function serve()
     {
-        $analytics = self::all();
+        $monitorings = self::all();
 
-        foreach ($analytics as $analytic) {
-            $parameter = str_replace(" ", "_", $analytic->parameter->name);
+        foreach ($monitorings as $monitoring) {
+            $parameter = str_replace(" ", "_", $monitoring->parameter->name);
 
-            switch ($analytic->method) {
+            switch ($monitoring->method) {
 
                 case "Latest":
-                    $data = Analysis::where("material_id", $analytic->material_id)
+                    $data = Analysis::where("material_id", $monitoring->material_id)
                         ->whereNotNull($parameter)
                         ->latest()
                         ->value($parameter) ?? null;
                     break;
 
                 case "Average":
-                    $data = Analysis::where("material_id", $analytic->material_id)
+                    $data = Analysis::where("material_id", $monitoring->material_id)
                         ->whereBetween("created_at", [session('from_datetime'), session('to_datetime')])
                         ->avg($parameter) ?? null;
                     break;
 
                 case "Minimum":
-                    $data = Analysis::where("material_id", $analytic->material_id)
+                    $data = Analysis::where("material_id", $monitoring->material_id)
                         ->whereBetween("created_at", [session('from_datetime'), session('to_datetime')])
                         ->min($parameter) ?? null;
                     break;
 
                 case "Maximum":
-                    $data = Analysis::where("material_id", $analytic->material_id)
+                    $data = Analysis::where("material_id", $monitoring->material_id)
                         ->whereBetween("created_at", [session('from_datetime'), session('to_datetime')])
                         ->max($parameter) ?? null;
                     break;
 
                 case "Summary":
-                    $data = Analysis::where("material_id", $analytic->material_id)
+                    $data = Analysis::where("material_id", $monitoring->material_id)
                         ->whereBetween("created_at", [session('from_datetime'), session('to_datetime')])
                         ->sum($parameter) ?? null;
                     break;
 
                 case "Count":
-                    $data = Analysis::where("material_id", $analytic->material_id)
+                    $data = Analysis::where("material_id", $monitoring->material_id)
                         ->whereBetween("created_at", [session('from_datetime'), session('to_datetime')])
                         ->count() ?? null;
                     break;
@@ -71,7 +97,7 @@ class Monitoring extends Model
                 case "Trendline":
                     $from = session('from_datetime');
                     $to = session('to_datetime');
-                    $data = Analysis::where("material_id", $analytic->material_id)
+                    $data = Analysis::where("material_id", $monitoring->material_id)
                         ->whereBetween("created_at", [$from, $to])
                         ->whereNotNull($parameter)
                         ->orderBy("created_at")
@@ -84,9 +110,9 @@ class Monitoring extends Model
                     break;
             }
 
-            $analytic->data = $data;
+            $monitoring->data = $data;
         }
 
-        return $analytics;
+        return $monitorings;
     }
 }
